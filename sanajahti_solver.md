a31bbe5e7f6bacbcf43d1173e6154d832b1f9cab
Jaan Tollander <jaan.tollander@gmail.com>
2018-04-08 14:14:12 +0300

    Sanajahti solver script scetch

--- sanajahti_solver.md
+++ sanajahti_solver.md
@@ -1,15 +1,15 @@
 # Sanajahti Solver
 
 ```
-index:  0 1 2 3 | 4 5 6 7 | 8 9 10 11  | 12 13 14 15
-string: A B C D | E F G H | I J  K  L  | M  N  O  P
-```
-
-```
 A B C D
 E F G H
 I J K L
 M N O P
+```
+
+```
+index:  0 1 2 3 | 4 5 6 7 | 8 9 10 11  | 12 13 14 15
+string: A B C D | E F G H | I J  K  L  | M  N  O  P
 ```
 
 Vertically
--- sanajahti_solver.sh
+++ sanajahti_solver.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Originally it is a game, known in English as Wordz. The game
 # play is connecting letters to words on 4x4 matrix of letters.
 # In any direction, any connection be it edge or corner
@@ -19,3 +21,81 @@
 # The script must accept a dictionary file as an argument.
 #
 # Example: ./sj_solver -d words.txt DOBEWCMEEETPCNX
+
+# -- Arguments and options --
+# Source of the getopt template: http://scicomp.aalto.fi/training/linux-shell-tutorial.html#working-with-the-input
+# here is the whole trick: getopt validates the input parameters, returns the
+# correct ones then they are reassigned back to $@ with 'set --'
+opts=$(getopt "d:" "$@") || exit 1   # instead of exit, can be 'usage' message/function
+set -- $opts
+
+# note: in one line one can do it like, though ugly
+#set -- $(getopt "sdf:" "$@" || kill -HUP $$)
+# $( ... || exit) does not work, since exit from inside a subshell, closes the # subshell only
+
+# since script input parameters have been validated and structured, we can go
+# through them we start an endless while and go through $@ with 'case' one by
+# one 'shift' makes another trick, every time it is invoked, it shifts down $@
+# params, $2 becomes $1, $2 becomes $3, etc while old $1 is unset getopt adds
+# -- to $@ which separates valid options and the rest that did not qualify
+while :; do
+  case ${1} in
+    -d) shift; dictionary_file=$1 ;; # path to the input file
+    --) shift; break ;;   # remove --
+  esac
+  shift
+done
+# by now $@ has only rubish filtered out by 'getopt', could be a file name
+
+
+# -- The algorithm --
+
+# Dictionary file should be supplied.
+[[ -f dictionary_file ]] || { exit 1; }
+
+dictionary=$(cat dictionary_file)
+string=$1
+
+# Input should be 16 characters long and only contain alphabetic characters
+# otherwise exit the program.
+[[ ${#string} != 16 ]] || { exit 1; }
+
+# Potential words
+words=''
+
+# Function that constructs potential words from indices of the string.
+construct_words() {
+  indices=$1
+  word=''
+  for i in $indices; do
+    word+=${string:$i}
+  done
+  words+=word
+  words+=" "
+  # Contiguous substrings
+  if [[ ${#word} == 4 ]]; then
+    words+=${word:0:3}
+    words+=" "
+    words+=${word:1:3}
+    words+=" "
+  fi
+}
+
+# Find words vertically.
+$(construct_words "0 1 2 3")
+
+# Find words horizontally.
+
+
+# Find words diagonally (up-right -> down-left).
+
+
+# Find words diagonally (down-right -> up-left).
+
+
+# Test is a potential words is found in the dictionary and output it in to
+# the STDIN.
+for word in $words; do
+  echo words | grep -E "\^$word\$"
+  # TODO: output
+done
